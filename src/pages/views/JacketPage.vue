5197da7a7e4fe91ddf7d3068f63bc002db400c68
Admin <hungbe0of@gmail.com>
2026-03-05 20:52:12 +0700

    Update JacketPage

--- src/pages/views/JacketPage.vue
+++ src/pages/views/JacketPage.vue
@@ -61,12 +61,6 @@
           </div>
 
           <div v-else-if="errorMsg" class="error-state">
-            <div class="error-icon"><svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <circle cx="12" cy="12" r="10"></circle>
-                <line x1="12" y1="8" x2="12" y2="12"></line>
-                <line x1="12" y1="16" x2="12.01" y2="16"></line>
-              </svg></div>
-            <p>{{ errorMsg }}</p>
             <button @click="fetchFilteredData" class="btn-retry">Thử lại</button>
           </div>
 
